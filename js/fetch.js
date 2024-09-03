7b29524abae08d60b3a5cf92850b5fa029d02ce5
infobiblioteca <infobiblioteca2024@gmail.com>
2024-09-03 11:23:16 -0400

    Update fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let tesisData = []; // Para almacenar los datos de tesis
 
     fetch('tesis.json')
@@ -20,12 +20,36 @@ document.addEventListener('DOMContentLoaded', function() {
         listaTesisContainer.innerHTML = ''; // Limpiar la tabla existente
         data.forEach(tesis => {
             const row = document.createElement('tr');
+    
+            // Generar el contenido de la columna "Observaciones"
+            const observaciones = tesis["OBSERVACIONES"] || '';
+
+            // Verificar si hay URLs en las observaciones para la columna de recursos
+            let recursos = '';
+            if (tesis["RECURSOS"]) {
+                const docs = tesis["RECURSOS"].split(';'); // Suponiendo que las URLs están separadas por punto y coma
+                docs.forEach(doc => {
+                    if (doc.trim().startsWith('http')) {
+                        recursos +=  `
+                        <a href="${doc.trim()}" target="_blank" style="display: inline-block; margin-right: 5px;">
+                        <i class="bi bi-file-earmark-text" style="font-size: 1.5rem; color: #236259;"></i>
+                    </a>`;
+                    }
+                });
+            }
+
+            // Mostrar un mensaje si no hay recursos
+            if (!recursos) {
+                recursos = '<span>Solo en formato físico</span>';
+            }
+
             row.innerHTML = `
                 <td>${tesis["Nª DE INV."]}</td>
                 <td>${tesis["NOMBRES Y APELLIDOS"]}</td>
                 <td>${tesis["TITULO"]}</td>
                 <td>${tesis["AÑO"]}</td>
-                <td>${tesis["OBSERVACIONES"]}</td>
+                <td>${observaciones}</td> <!-- Ahora correctamente definida -->
+                <td>${recursos}</td>
             `;
             listaTesisContainer.appendChild(row);
         });
@@ -51,15 +75,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const searchInput = document.getElementById('search');
     const searchForm = document.getElementById('search-form');
 
-    searchInput.addEventListener('input', function() {
+    searchInput.addEventListener('input', function () {
         const selectedCriteria = searchForm.querySelector('input[name="searchCriteria"]:checked').value;
         filterTable(this.value, selectedCriteria);
     });
 
     // Evento de cambio para los botones de radio
-    searchForm.addEventListener('change', function() {
+    searchForm.addEventListener('change', function () {
         // Limpiar el campo de búsqueda y disparar la búsqueda con el criterio actualizado
         searchInput.value = '';
         searchInput.dispatchEvent(new Event('input'));
     });
 });
+
